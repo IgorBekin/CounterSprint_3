93f3f52ca631549d3830d75c835141afd86d8842
igorBekin <igor_bekin@mail.ru>
2023-02-21 11:22:00 +0300

    изп

--- CounterSprint_3/ViewController.swift
+++ CounterSprint_3/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var App = 0
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var count: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
     }
 
 
+    @IBAction func App(_ sender: Any) {
+        App += 1
+        print(App)
+        count.text = "Значение счетчика: \(App)"
+        
+    }
 }
 
